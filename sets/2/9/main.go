fbd41cdb80850309a0a2f452ded02598620b3ebd
Nolan1324 <nkuza8@gmail.com>
2025-04-27 13:42:52 -0400

    Change strconv.Quote to %q

--- sets/2/9/main.go
+++ sets/2/9/main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"cryptopals/internal/cipherx"
 	"fmt"
-	"strconv"
 )
 
 func main() {
 	output := cipherx.Pcks7Padding([]byte("YELLOW SUBMARINE"), 20)
-	fmt.Printf("%s\n", strconv.Quote(string(output)))
+	fmt.Printf("%q\n", output)
 }
