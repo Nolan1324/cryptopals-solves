8a0bd091a5b0ca6cb3a412647c0ee3291fa1732b
Nolan1324 <nkuza8@gmail.com>
2025-04-28 14:23:49 -0400

    Refactor challenge 12 oracle

--- sets/2/12/main.go
+++ sets/2/12/main.go
@@ -2,44 +2,12 @@ package main
 
 import (
 	"bytes"
-	"cryptopals/internal/cipherx"
 	"cryptopals/internal/crack"
-	"cryptopals/internal/randx"
 	"encoding/base64"
 	"fmt"
 	"log"
 	"os"
 )
-
-type Ch12Oracle struct {
-	key       []byte
-	plaintext []byte
-}
-
-func makeOracle(plaintext []byte, keySize int) Ch12Oracle {
-	key := randx.RandBytes(keySize)
-	return Ch12Oracle{key: key, plaintext: plaintext}
-}
-
-func (o Ch12Oracle) Encrypt(userPlaintext []byte) []byte {
-	const bs = 16
-
-	buf := make([]byte, 0, len(userPlaintext)+len(o.plaintext)+bs)
-	buf = append(buf, userPlaintext...)
-	buf = append(buf, o.plaintext...)
-	buf = cipherx.Pcks7Padding(buf, bs)
-
-	output, err := cipherx.EncryptAesEcb(buf, o.key)
-	if err != nil {
-		panic(err)
-	}
-
-	return output
-}
-
-func (o Ch12Oracle) CheckAnswer(answer []byte) bool {
-	return bytes.Equal(answer, o.plaintext)
-}
 
 func crackBlockSize(o Ch12Oracle) int {
 	buf := make([]byte, 256)
---
+++ sets/2/12/oracle.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"cryptopals/internal/cipherx"
+	"cryptopals/internal/randx"
+)
+
+type Ch12Oracle struct {
+	key       []byte
+	plaintext []byte
+}
+
+func makeOracle(plaintext []byte, keySize int) Ch12Oracle {
+	key := randx.RandBytes(keySize)
+	return Ch12Oracle{key: key, plaintext: plaintext}
+}
+
+func (o Ch12Oracle) Encrypt(userPlaintext []byte) []byte {
+	const bs = 16
+
+	buf := make([]byte, 0, len(userPlaintext)+len(o.plaintext)+bs)
+	buf = append(buf, userPlaintext...)
+	buf = append(buf, o.plaintext...)
+	buf = cipherx.Pcks7Padding(buf, bs)
+
+	output, err := cipherx.EncryptAesEcb(buf, o.key)
+	if err != nil {
+		panic(err)
+	}
+
+	return output
+}
+
+func (o Ch12Oracle) CheckAnswer(answer []byte) bool {
+	return bytes.Equal(answer, o.plaintext)
+}
