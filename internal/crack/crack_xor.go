c0754aedf3e9e611bd6b77858d0563f602e07135
Nolan1324 <nkuza8@gmail.com>
2025-04-30 21:14:41 -0400

    Removed unused variable

--- internal/crack/crack_xor.go
+++ internal/crack/crack_xor.go
@@ -4,19 +4,7 @@ import (
 	"cryptopals/internal/cipherx"
 	"cryptopals/internal/histogram"
 	"sort"
-
-	"gonum.org/v1/gonum/mat"
 )
-
-var frequencyVec *mat.VecDense
-
-func initFrequencyVec() {
-	if frequencyVec != nil {
-		return
-	}
-	var frequencies = [26]float64{0.082, 0.015, 0.028, 0.043, 0.127, 0.022, 0.02, 0.061, 0.07, 0.0015, 0.0077, 0.04, 0.024, 0.067, 0.075, 0.019, 0.00095, 0.06, 0.063, 0.091, 0.028, 0.0098, 0.024, 0.0015, 0.02, 0.00074}
-	frequencyVec = mat.NewVecDense(26, frequencies[:])
-}
 
 func CrackSingleXor(buf []byte) ([]byte, byte, float64) {
 	var bestGuess []byte
