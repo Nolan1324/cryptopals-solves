cc11d4fc2c6a2f75268305605aea369e66073ad6
Nolan1324 <nkuza8@gmail.com>
2025-05-16 14:52:27 -0400

    Add run all option to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
+set -e
 
 if [ -z "$1" ]; then
   echo "Error: no argument provided"
   exit 1
+fi
+
+if [ "$1" = "all" ]; then
+  i=1
+  while true; do
+    dir=$(find sets/*/. -name "$i")
+    if [ -z "$dir" ]; then
+      break
+    fi
+    echo ""
+    echo "===== Running challenge $i in $dir ====="
+    (cd "$dir" && go run .)
+    i=$((i + 1))
+  done
+  exit 0
 fi
 
 dir=$(find sets/*/. -name "$1")
